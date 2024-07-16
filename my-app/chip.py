6d5890607060b77fbbe4debe92beb23624cd3748
drinkincode] <jonnysmith696910@gmail.com>
2024-07-16 00:32:01 -0400

    working chat and bingo game

--- my-app/chip.py
+++ my-app/chip.py
@@ -1,12 +1,12 @@
 import flet as ft
 class Chip(ft.GestureDetector):
-    def __init__(self):
+    def __init__(self, left, top):
         self.piece = super().__init__(
             mouse_cursor=ft.MouseCursor.MOVE,
             drag_interval=10,
             on_vertical_drag_update=self.drag,
-            left=100,
-            top=100,
+            left = left,
+            top =  top,
             content=ft.Container(opacity=0.65, bgcolor=ft.colors.BLUE, width=50, height=50),
         )
     def drag(self, e: ft.DragUpdateEvent):
--- my-app/main.py
+++ my-app/main.py
@@ -1,41 +1,70 @@
 import flet as ft
 from chip import Chip
-from slot import Slot
-
+# from slot import Slot
+import csv
+from chat import *
 
 def main(page: ft.Page):
-    CHIP_TOTAL = 25
-    stack = []
-    text_list = ['take a sip', 'take a sip', 'take a sip', 'take a sip', 'take a sip',
-                 'take a sip', 'take a sip', 'take a sip', 'take a sip', 'take a sip', 
-                 'take a sip', 'take a sip', 'Free', 'take a sip', 'take a sip', 
-                 'take a sip', 'take a sip', 'take a sip', 'take a sip', 'take a sip',
-                 'take a sip', 'take a sip', 'take a sip', 'take a sip', 'take a sip'
-                ]
     
-    board_top = 200
-    board_left = 150
+    chat_bar_top = 800
+    chat_bar_left = 1500
+    CHIP_TOTAL = 25
+    CONTAINER_LENGTH = 50
+    
+    CONTAINER_LEFT = 50
+    
+    container_top = 50
+    curr_container_left = CONTAINER_LEFT
+    
+    stack = []
+    
+    text_list = []
+    with open('bingo_words.csv', mode ='r')as file:
+        csvFile = csv.reader(file)
+        for line in csvFile:
+                text_list.append(line)
+                
+    # chip_top = 100
+    # chip_left = (CONTAINER_LEFT + chip_top) * (CHIP_TOTAL // 5)    
+    
+    def on_click(e):
+        
+        if e.control.bgcolor == "cyan":
+            e.control.bgcolor = "amber"
+        else:
+            e.control.bgcolor = "cyan"
+            
+        page.update()
+        
     count = 0
+    
     for i in range(CHIP_TOTAL//5):
         for j in range(CHIP_TOTAL//5):
             slot = ft.Container(
-                content=ft.Text(text_list[count]),
-                top=board_top, 
-                left=board_left, 
-                width=50, 
-                height=50, 
+                content=ft.Text(text_list[count][0]),
+                top=container_top, 
+                bgcolor='cyan',
+                on_click = on_click,
+                left=curr_container_left, 
+                width=CONTAINER_LENGTH,
+                height=CONTAINER_LENGTH, 
                 border=ft.border.all(1)
             )
             stack.append(slot)
-            board_left += 150
+            curr_container_left += CONTAINER_LENGTH
             count+=1
-        board_top += 150    
-        board_left = 150
+        curr_container_left = CONTAINER_LEFT
+        container_top += CONTAINER_LENGTH    
         
-    for i in range(CHIP_TOTAL):
-        chip = Chip()
-        stack.append(chip)
         
-    page.add( ft.Stack(stack, width=1000, height=1000))
+# Chips instead of highlight
+    # for i in range(CHIP_TOTAL):
+    #     chip = Chip(chip_left, chip_top)
+    #     stack.append(chip)
+    bingo = ft.Stack(stack, width=300, height=300)
+    layout = ft.Row(controls = [ft.Column(controls=[bingo])])
+    layout_with_chat = player_chat(page, layout)
 
+    page.add(layout_with_chat)
+    
 ft.app(target=main)
