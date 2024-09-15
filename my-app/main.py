c773ca3a3818f7ca48e19b032efa5ab4b9df16d4
drinkincode] <jonnysmith696910@gmail.com>
2024-09-15 16:52:25 -0500

    remove nav from main

--- my-app/main.py
+++ my-app/main.py
@@ -1,12 +1,12 @@
 import flet as ft
 from bingo import Bingo
-from nav import Nav
+# from nav import Nav
 import csv
 from chat import *
 
 def main(page: ft.Page):
     page.adaptive = True
-    page.navigation_bar = Nav()
+    # page.navigation_bar = Nav()
     bingo_game = Bingo(page)
     layout = ft.Column(controls = [ft.Column(controls=[bingo_game])])
     chat = player_chat(page)
@@ -15,3 +15,4 @@ def main(page: ft.Page):
     page.add(layout)
     
 ft.app(target=main)
+# ft.app(target=main, view=ft.AppView.WEB_BROWSER)
